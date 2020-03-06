ac1cd520409b696d73dd73ac2dd0b5e5ce46a0de
codingpoem <36836245+codingpoem@users.noreply.github.com>
2020-03-06 14:59:01 +0800

    Update PackSearchXref.py

--- PackSearchXref.py
+++ PackSearchXref.py
@@ -5,16 +5,22 @@ from com.pnfsoftware.jeb.core.units.code.android import IApkUnit, IDexUnit
 from com.pnfsoftware.jeb.core.actions import ActionContext
 from com.pnfsoftware.jeb.core.actions import ActionXrefsData
 from com.pnfsoftware.jeb.core.actions import Actions
+
 # from com.pnfsoftware.jeb.core.units.code import ICodeUnit
 # from com.pnfsoftware.jeb.core import RuntimeProjectUtil
+'''
+function:find the outside cross-reference of a package
+usage:keep caret in hierarchy,execute this script
+environment:jeb3.0 or jeb3.14
+'''
 
-
-class PackSearchXref(IScript):
+class PackageXref(IScript):
     ICodeItems = []
     rows = []
     base_dir = 'hello'
 
     def run(self, ctx):
+
         if not isinstance(ctx, IGraphicalClientContext):
             print('This script must be run within a graphical client')
             return
@@ -46,13 +52,19 @@ class PackSearchXref(IScript):
             self.getCroccReference(item, focus_unit)
 
         #3-show results in box
-        self.showBox(ctx)
+        if "displayList" in dir(ctx):
+            self.showBox(ctx)
+        else:
+            pass
+            # print("---all cross_ref-------")
+            # for row in self.rows:
+            #     print(row)
 
 
     def GenChilds(self, Item):
         # type = str(Item).split(':')[0][1:]
-        print(Item)
-        print(Item.toString())
+        # print(Item)
+        # print(Item.toString())
         type = Item.toString().split(':')[0][1:]
         if type == 'DexPackage':
             for child in Item.getChildren():
@@ -69,24 +81,38 @@ class PackSearchXref(IScript):
 
     def getCroccReference(self, ICodeItem, unit):
         data = ActionXrefsData()
-        index = 0
+        num = 0
         if unit.prepareExecution(ActionContext(unit, Actions.QUERY_XREFS, ICodeItem.getItemId(), ICodeItem.getAddress()),data):
             addrs = data.getAddresses()
             details = data.getDetails()
 
-            for i in range(0,len(details)):
-                if details[i] != "Not a direct reference" and addrs[i].find(self.base_dir) != 1 :
-                    if index == 0:
-                        print("Cross-Reference:", ICodeItem.toString())
-                    index += 1
-                    print(addrs[i])
-                    self.rows.append([addrs[i], ICodeItem.getAddress()])
+            if details:
+                ## work for demo version jeb-demo-3.14.0.202002252048
+                for i in range(0,len(details)):
+                    if details[i] != "Not a direct reference" and addrs[i].find(self.base_dir) != 1 :
+                        if num == 0:
+                            print("Cross-Reference:", ICodeItem.toString())
+                        num += 1
+                        print(addrs[i])
+                        self.rows.append([addrs[i], ICodeItem.getAddress()])
+
+            else:
+                ### work for beta version JEB3.0.0.201808031948 Beta
+                for xref_addr in addrs:
+                    # print("--index:",addrs.index(xref_addr))
+                    if xref_addr.find(self.base_dir) != 1:
+                        if num == 0:
+                            print("Cross-Reference:",ICodeItem.toString())
+                        num += 1
+                        print(xref_addr)
+                        self.rows.append([xref_addr])
+
         return None
 
 
     def showBox(self, ctx):
         box_caption = "cross-ref"
-        box_mess = "this is optional message"
+        box_mess = "outside cross-references of "+'"'+self.base_dir+'"'
         box_headers = ["address", "address_by"]
         index = ctx.displayList(box_caption, box_mess, box_headers, self.rows)
 
